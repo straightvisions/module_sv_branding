ac6182b09b85593b43b4de3e6e52e09e944ad6b6
Matthias Bathke <matthias-bathke@straightvisions.com>
2020-04-21 15:51:12 +0200

    UPDATE

--- lib/frontend/css/config.php
+++ lib/frontend/css/config.php
@@ -11,7 +11,7 @@
 	}
 
 	if($script->get_parent()->has_logo()){
-		require($script->get_parent()->get_path( 'lib/frontend/css/config/logo.php' ));
-	}else{
-		require($script->get_parent()->get_path( 'lib/frontend/css/config/title.php' ));
-	}
+	require($script->get_parent()->get_path( 'lib/frontend/css/config/logo.php' ));
+}else{
+	require($script->get_parent()->get_path( 'lib/frontend/css/config/title.php' ));
+}
