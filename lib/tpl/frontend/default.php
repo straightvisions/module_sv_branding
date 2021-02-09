13c03a535397ae69e600835be72137ce4e328b64
Matthias Bathke <matthias-bathke@straightvisions.com>
2021-02-09 19:40:26 +0100

    FIX WARNING

--- lib/tpl/frontend/default.php
+++ lib/tpl/frontend/default.php
@@ -8,8 +8,11 @@
 					$custom_logo_id		= get_theme_mod( 'custom_logo' );
 					//var_dump($custom_logo_id);
 					$logo				= wp_get_attachment_url( $custom_logo_id );
-					$width				= intval(str_replace('px','',reset($this->get_setting('logo_width')->get_data())));
-					$height				= intval(str_replace('px','',reset($this->get_setting('logo_height')->get_data())));
+					$logo_width			= $this->get_setting('logo_width')->get_data();
+					$logo_height		= $this->get_setting('logo_height')->get_data();
+
+					$width				= intval(str_replace('px','',reset($logo_width)));
+					$height				= intval(str_replace('px','',reset($logo_height)));
 
 					echo '<a href="' . get_home_url() . '"><img src="' . esc_url( $logo ) . '" alt="' . get_bloginfo( 'name' ) . '" width="'.($width > 1 ? $width : '').'" height="'.($height > 1 ? $height : '').'"></a>';
 				} else {
