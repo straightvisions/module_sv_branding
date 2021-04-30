3316754fa13c319a60ee38c6ba1d0476e79da28b
Matthias Bathke <matthias-bathke@straightvisions.com>
2021-04-30 17:39:04 +0200

    FIX WARNING

--- lib/tpl/frontend/default.php
+++ lib/tpl/frontend/default.php
@@ -6,13 +6,15 @@
 					echo '<a href="' . get_home_url() . '"><img src="' . get_header_image() . '" alt="' . get_bloginfo( 'title' ) . '" /></a>';
 				} elseif ( get_custom_logo() ) {
 					$custom_logo_id		= get_theme_mod( 'custom_logo' );
-					//var_dump($custom_logo_id);
+
 					$logo				= wp_get_attachment_url( $custom_logo_id );
 					$logo_width			= $this->get_setting('logo_width')->get_data();
 					$logo_height		= $this->get_setting('logo_height')->get_data();
+					$logo_width			= reset($logo_width);
+					$logo_height		= reset($logo_height);
 
-					$width				= intval(str_replace('px','',reset($logo_width)));
-					$height				= intval(str_replace('px','',reset($logo_height)));
+					$width				= intval(str_replace('px','', $logo_width));
+					$height				= intval(str_replace('px','', $logo_height));
 
 					echo '<a href="' . get_home_url() . '"><img src="' . esc_url( $logo ) . '" alt="' . get_bloginfo( 'name' ) . '" width="'.($width > 1 ? $width : '').'" height="'.($height > 1 ? $height : '').'"></a>';
 				} else {
